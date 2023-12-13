346884ddc543798dc3176550e5982b64265cb914
Jorge Villalba Dickel <150525749+jvillalbadickel@users.noreply.github.com>
2023-12-13 08:56:36 -0300

    Update golden_age_videogames.sql

--- golden_age_videogames.sql
+++ golden_age_videogames.sql
@@ -1,3 +1,53 @@
+-- Create DB
+DROP TABLE game_sales;
+
+CREATE TABLE game_sales (
+  game VARCHAR(100) PRIMARY KEY,
+  platform VARCHAR(64),
+  publisher VARCHAR(64),
+  developer VARCHAR(64),
+  games_sold NUMERIC(5, 2),
+  year INT
+);
+
+DROP TABLE reviews;
+
+CREATE TABLE reviews (
+    game VARCHAR(100) PRIMARY KEY,
+    critic_score NUMERIC(4, 2),   
+    user_score NUMERIC(4, 2)
+);
+
+DROP TABLE top_critic_years;
+
+CREATE TABLE top_critic_years (
+    year INT PRIMARY KEY,
+    avg_critic_score NUMERIC(4, 2)  
+);
+
+DROP TABLE top_critic_years_more_than_four_games;
+
+CREATE TABLE top_critic_years_more_than_four_games (
+    year INT PRIMARY KEY,
+    num_games INT,
+    avg_critic_score NUMERIC(4, 2)  
+);
+
+DROP TABLE top_user_years_more_than_four_games;
+
+CREATE TABLE top_user_years_more_than_four_games (
+    year INT PRIMARY KEY,
+    num_games INT,
+    avg_user_score NUMERIC(4, 2)  
+);
+
+\copy game_sales FROM 'game_sales.csv' DELIMITER ',' CSV HEADER;
+\copy reviews FROM 'game_reviews.csv' DELIMITER ',' CSV HEADER;
+\copy top_critic_years FROM 'top_critic_scores.csv' DELIMITER ',' CSV HEADER;
+\copy top_critic_years_more_than_four_games FROM 'top_critic_scores_more_than_four_games.csv' DELIMITER ',' CSV HEADER;
+\copy top_user_years_more_than_four_games FROM 'top_user_scores_more_than_four_games.csv' DELIMITER ',' CSV HEADER;
+
+
 -- Select all information for the top ten best-selling games
 -- Order the results from best-selling game down to tenth best-selling
 SELECT *
@@ -253,17 +303,3 @@ FROM TopSellingGames t
 JOIN CriticFavoredGames c ON t.game = c.game
 JOIN UserFavoredGames u ON t.game = u.game
 ORDER BY t.total_sales DESC, c.avg_critic_score DESC, u.avg_user_score DESC;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
